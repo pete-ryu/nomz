e17f146c3fd08cc208146713e6d799d66411f90f
tackypinkflamingos <ajstocchetti@gmail.com>
2015-11-20 10:42:18 -0500

    clean up fsg server files

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,7 @@
 'use strict';
 var Promise = require('bluebird');
-var path = require('path');
 var chalk = require('chalk');
 
-// var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 var DATABASE_URI = process.env.MONGOURI;
 
 var mongoose = require('mongoose');
