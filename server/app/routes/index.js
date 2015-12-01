3216ebd714e7de99fbb203ed1f46fe620ecb5960
nyczmagix <nyczmagix@MacBook-Pro-Retina.local>
2015-12-01 18:37:28 -0500

    Venue Results + Venue Details

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,14 +1,17 @@
 'use strict';
 var router = require('express').Router();
+var bodyParser = require('body-parser');
+
 module.exports = router;
 
 router.use('/user', require('./members'));
 router.use('/game', require('./game.js'));
 router.use('/venue', require('./venue.js'));
 router.use('/feed', require('./feed.js'));
+router.use('/recommend', require('./recommend.js'));
 
 // Make sure this is after all of
 // the registered routes!
-router.use(function (req, res) {
-    res.status(404).end();
+router.use(function(req, res) {
+  res.status(404).end();
 });
