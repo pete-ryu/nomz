970b12df5017e76b6ac14167f826ba52f02f10e4
tackypinkflamingos <ajstocchetti@gmail.com>
2015-12-09 00:38:29 -0500

    new start page

--- nomzClient/App/components/home-screen.js
+++ nomzClient/App/components/home-screen.js
@@ -1,5 +1,6 @@
 var React = require('react-native');
 
+var Button = require('./react-native-button');
 var Play = require('./game-play');
 
 var {
@@ -13,28 +14,33 @@ var {
 
 
 var Homescreen = React.createClass({
-  playGame(mealType) {
+  playGame() {
     this.props.navigator.push({
       title: 'Review Dishes',
       component: Play,
       backButtonTitle: ' '
-      // backButtonTitle: 'swipe'
+      // backButtonTitle: 'Main Menu'
     });
   },
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ marginTop: 65 }}></View>
-        <TouchableOpacity style={styles.box} onPress={()=> this.playGame() }>
-          <Text>{"Play Nomz!"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.box} onPress={()=> this.playGame() }>
-          <Text>{"Log in with Foursquare"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.box} onPress={()=> this.playGame() }>
-          <Text>{"Upload an Image"}</Text>
-        </TouchableOpacity>
+      <Image source={{ uri: "nomz" , isStatic: true }} style={styles.bgImg} />
+        <View style={{ marginTop: 65, backgroundColor: 'transparent' }}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}>
+          <Button
+            style={styles.btn}
+            onPress={this.playGame}>
+            {"Play Nomz!"}
+          </Button>
+          <Button
+            style={styles.btn}
+            onPress={this.playGame}>
+            {"Log in with Foursquare"}
+          </Button>
+        </View>
       </View>
     )
   }
@@ -47,8 +53,32 @@ var styles = StyleSheet.create({
     flexWrap: 'wrap'
   },
   box: {
-    flex: 1
-  }
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems:'center',
+
+  },
+
+  bgImg: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: 0.9
+  },
+
+  btn: {
+    flexDirection: 'row',
+    width: 300,
+    fontSize: 20,
+    color: 'white',
+    padding: 10,
+    marginBottom: 20,
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: '#007aff'
+  },
 })
 
 module.exports = Homescreen;
