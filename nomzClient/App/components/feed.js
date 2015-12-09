b80b7280de735cfd89cc24fc3d69baec2d71d4d1
IanAllen <ianavilallen@gmail.com>
2015-12-08 21:56:43 -0500

    started feed component

---
+++ nomzClient/App/components/feed.js
@@ -0,0 +1,28 @@
+var React = require('react-native');
+
+var {
+  Component,
+  View,
+  FlexView,
+  Image
+} = React;
+
+var api = require('./utils/api');
+
+class Feed extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+  getFeed() {
+
+  }
+
+  componentWillMount() {
+
+  }
+
+
+}
